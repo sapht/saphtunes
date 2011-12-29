dcd3815f53199a45d0ac72f7ff4201bf81fcc9d4
Daniel Hjerth <d.hjerth@gmail.com>
2011-12-29 10:36:11 +0100

    changed relative test config path

--- test/test_git.c
+++ test/test_git.c
@@ -5,6 +5,6 @@
 #include <dirent.h>
 
 int main(int argc, char **argv) {
-	struct git_config *config = git_config_from_file("test/git_config");
+	struct git_config *config = git_config_from_file("git_config");
 	printf("%s\n", git_origin_from_config(config));
 }
