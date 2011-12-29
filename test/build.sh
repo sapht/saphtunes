3cd7cd1bee2256f1bd1fddf9062c4de092990eab
Daniel Hjerth <d.hjerth@gmail.com>
2011-12-29 11:43:52 +0100

    changed gcc flags of test/build.sh

--- test/build.sh
+++ test/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-CFLAGS="-I../ -I/usr/local/include/cdk -lncurses -lcdk"
+CFLAGS="-std=c99 -ggdb -Wall -I../ -I/usr/local/include/cdk -lncurses -lcdk"
 gcc $CFLAGS test_cdk.c -o test_cdk.o
-CFLAGS="-I../"
+CFLAGS="-std=c99 -ggdb -Wall -I../"
 gcc $CFLAGS ../git.c test_git.c -o test_git.o
